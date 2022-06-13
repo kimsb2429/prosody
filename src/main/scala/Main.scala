d00b3d243f13d223cc3e4a82f26faab55c0a3d85
EC2 Default User <ec2-user@ip-172-31-87-252.ec2.internal>
2022-06-13 15:08:57 +0000

    bug fix

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import org.apache.log4j._
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.input_file_name
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 
 object Prosody extends App{
       override def main(args: Array[String]): Unit = { 
