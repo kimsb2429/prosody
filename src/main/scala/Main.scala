9fddc5bf5d191ba5e6ee1b2e3578a49e9e9f2f17
EC2 Default User <ec2-user@ip-172-31-87-252.ec2.internal>
2022-06-19 17:25:19 +0000

    Testing Pincelate

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -30,7 +30,7 @@ object Prosody extends App{
         // Get spark context
         // val sc = spark.sparkContext
         // sc.addFile(soundoutScript)
-        val soundoutScriptPath = "/home/ec2-user/" + soundoutScript
+        val soundoutScriptPath = "/home/hadoop/" + soundoutScript
         val soundoutCmd = Seq("sudo", "python3", soundoutScriptPath)
         // sc.addFile(soundoutScriptPath) 
 
